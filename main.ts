0319889cc286937a5df4b33741c2d45149c0b0ee
SamuelMartosCarmet <124789327+SamuelMartosCarmet@users.noreply.github.com>
2023-05-16 11:24:50 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,49 +1,77 @@
 function zigzag () {
-	
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    basic.pause(1000)
+    for (let index = 0; index < 2; index++) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(2000)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(2000)
+    }
+    maqueen.motorStop(maqueen.Motors.All)
 }
 function cuadrado () {
-    crono()
-    while (false) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 106)
+    for (let index = 0; index < 4; index++) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(2000)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        basic.pause(600)
     }
+    maqueen.motorStop(maqueen.Motors.All)
 }
 input.onButtonPressed(Button.A, function () {
     if (ejecutar == "c") {
         ejecutar = "v"
     } else if (ejecutar == "v") {
-        ejecutar = "z"
-    } else if (ejecutar == "z") {
         ejecutar = "s"
     } else if (ejecutar == "s") {
+        ejecutar = "z"
+    } else if (ejecutar == "z") {
         ejecutar = "c"
     }
+    basic.showString(ejecutar)
 })
-function crono () {
-    tiempo = 3000
-    while (false) {
-    	
-    }
-}
 function circulo () {
-	
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    basic.pause(5000)
+    maqueen.motorStop(maqueen.Motors.All)
 }
-function eses () {
-	
-}
-let tiempo = 0
-let ejecutar = ""
-ejecutar = "c"
-basic.forever(function () {
+input.onButtonPressed(Button.B, function () {
     if (ejecutar == "c") {
         cuadrado()
-    }
-    if (ejecutar == "v") {
+    } else if (ejecutar == "v") {
         circulo()
-    }
-    if (ejecutar == "z") {
+    } else if (ejecutar == "s") {
+        eses()
+    } else if (ejecutar == "z") {
         zigzag()
     }
-    if (ejecutar == "s") {
-        eses()
-    }
 })
+function eses () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    basic.pause(1000)
+    for (let index = 0; index < 2; index++) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        basic.pause(3000)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+        basic.pause(3000)
+    }
+    maqueen.motorStop(maqueen.Motors.All)
+}
+let ejecutar = ""
+// Pongo esta variable porque me da pereza escribir siempre el mismo número
+ejecutar = "c"
+basic.showString(ejecutar)
